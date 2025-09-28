bb23a0bc08a390fbb330121322edbc217e845f7f
MohammadAminSohele <mohammadaminsohele@gmail.com>
2025-09-28 19:41:04 +0330

    add help command to hello_world function and add message_on_pattern_detected with regex arg , content type

--- hello_world.py
+++ hello_world.py
@@ -2,11 +2,19 @@ from telebot import TeleBot
 
 bot = TeleBot('7951234207:AAE1keTBO3b2E6dqWAwWaFVUv3lIV4IE_9Y')
 
-@bot.message_handler(['start'])
+@bot.message_handler(['start', 'help'])
 
 
 def hello_world(message):
-    bot.send_message(message.chat.id, 'Hello,World')
-    print(message)
+    global id 
+    id = message.chat.id
+    if message.text == '/start':
+        bot.send_message(id, 'Hello,World')
+    elif message.text == '/help':
+        bot.send_message(id, 'do you need help?')
+
+@bot.message_handler(content_types=['text'], regexp=r'\bMohammad\b')
+def message_on_pattern_detected(message):
+    bot.send_message(id, 'Nice to meet you')
 
 bot.polling()
