b54ddb2df63901ddb5ea9e008a6802dc10a29c69
MohammadAminSohele <mohammadaminsohele@gmail.com>
2025-12-20 18:37:34 +0330

    Update

---
+++ query.py
@@ -0,0 +1,69 @@
+import sqlite3
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+bot_db = os.getenv('bot_db')
+
+def connect():
+    conn = sqlite3.connect(bot_db)
+    cursor = conn.cursor()
+    return conn , cursor
+
+def insert_user(user_id, username):
+    conn ,cursor = connect()
+
+    cursor.execute("INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)", (user_id, username))
+
+    conn.commit()
+    conn.close()
+
+def get_services():
+    conn, cursor = connect()
+
+    cursor.execute("SELECT service_id, name FROM services")
+    services = cursor.fetchall()
+
+    conn.close()
+    return services
+
+def get_dates(service_id):
+    conn, cursor  = connect()
+
+    cursor.execute("SELECT DISTINCT date FROM slots WHERE service_id = ?", (service_id,))
+    dates = [row[0] for row in cursor.fetchall()]
+
+    conn.close()
+    return dates
+
+def get_times(service_id, date):
+    conn, cursor  = connect()
+
+    cursor.execute("""
+        SELECT slot_id, time FROM slots
+        WHERE service_id = ? AND date = ? AND status = 'available'
+        ORDER BY time ASC
+    """, (service_id, date))
+    times = cursor.fetchall()
+
+    conn.close()
+    return times
+
+def insert_service(name, admin_id):
+    conn, cursor  = connect()
+    cursor.execute("INSERT INTO services (name, admin_id) VALUES (?, ?)", (name, admin_id))
+    service_id = cursor.lastrowid
+    
+    conn.commit()
+    conn.close()
+    return service_id
+
+def insert_slots(slot_id,date,times):
+    conn,cursor = connect()
+    for time in times:
+        cursor.execute(
+            'INSERT INTO slots(slot_id,date,time,status) VALUES(?,?,?,?)',(slot_id,date,time,'available')
+        )
+
+    conn.commit()
+    conn.close()
---
+++ schema.py
@@ -0,0 +1,46 @@
+import sqlite3
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+BOT_DB = os.getenv('BOT_DB')
+
+conn = sqlite3.connect(BOT_DB)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id TEXT PRIMARY KEY,
+    username TEXT
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS services (
+    service_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    admin_id TEXT
+)
+""")
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS slots (
+    slot_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    service_id INTEGER,
+    date TEXT,
+    time TEXT,
+    status TEXT DEFAULT 'available'
+)
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS appointments (
+    appointment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
+    slot_id INTEGER
+)
+""")
+
+conn.commit()
+conn.close()
